17b9cb8b9aacab97657257856a0ac42b9e99092d
Aman Katana <amanbek.kud@gmail.com>
2024-03-04 00:14:07 +0300

    LeetCode 704. Binary Search

--- LeetCode/Easy.py
+++ LeetCode/Easy.py
@@ -1,25 +1,20 @@
 # 704. Binary Search ==================================================================================================
-# import math
-#
-#
-# class Solution:
-#     def search(self, nums, target):
-#         """
-#         :type nums: List[int]
-#         :type target: int
-#         :rtype: int
-#         """
-#         start = 0
-#         end = len(nums) - 1
-#         while start <= end:
-#             mid = int(math.ceil((start + end) / 2))
-#
-#             guess = nums[mid]
-#             if guess == target:
-#                 return mid
-#             elif guess > target:
-#                 end = mid - 1
-#             else:
-#                 start = mid + 1
-#         else:
-#             return -1
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+            guess = nums[mid]
+            if guess == target:
+                return mid
+            elif guess > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
