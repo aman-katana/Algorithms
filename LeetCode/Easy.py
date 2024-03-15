89c533c412266fbce35fb1284ff73787f35722c1
Aman Katana <amanbek.kud@gmail.com>
2024-03-15 12:01:57 +0300

    LeetCode 551. Student Attendance Record I

--- LeetCode/Easy.py
+++ LeetCode/Easy.py
@@ -1,20 +1,42 @@
 # 704. Binary Search ==================================================================================================
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        start, end = 0, len(nums) - 1
+# class Solution(object):
+#     def search(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: int
+#         """
+#         start, end = 0, len(nums) - 1
+#
+#         while start <= end:
+#             mid = (start + end) // 2
+#             guess = nums[mid]
+#             if guess == target:
+#                 return mid
+#             elif guess > target:
+#                 end = mid - 1
+#             else:
+#                 start = mid + 1
+#         return -1
 
-        while start <= end:
-            mid = (start + end) // 2
-            guess = nums[mid]
-            if guess == target:
-                return mid
-            elif guess > target:
-                end = mid - 1
+# 551. Student Attendance Record I
+class Solution(object):
+    def checkRecord(self, s: str):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        absent = 0
+        late = 0
+        for i in s:
+            if i == 'A':
+                absent += 1
+                late = 0
+            elif i == 'L':
+                late += 1
             else:
-                start = mid + 1
-        return -1
+                late = 0
+            if late == 3 or absent > 1:
+                return False
+        else:
+            return True
