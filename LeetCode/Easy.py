98e169130730848f5f1911d3f7f89138b86652f6
Aman Katana <amanbek.kud@gmail.com>
2024-03-16 21:18:06 +0300

    LeetCode 977. Squares of a Sorted Array

--- LeetCode/Easy.py
+++ LeetCode/Easy.py
@@ -20,18 +20,34 @@
 #         return -1
 
 # 551. Student Attendance Record I
-class Solution:
-    def checkRecord(self, s: str) -> bool:
-        absent = late = 0
-        for i in s:
-            if i == 'A':
-                absent += 1
-                late = 0
-            elif i == 'L':
-                late += 1
-            else:
-                late = 0
-            if late == 3 or absent > 1:
-                return False
+# class Solution:
+#     def checkRecord(self, s: str) -> bool:
+#         absent = late = 0
+#         for i in s:
+#             if i == 'A':
+#                 absent += 1
+#                 late = 0
+#             elif i == 'L':
+#                 late += 1
+#             else:
+#                 late = 0
+#             if late == 3 or absent > 1:
+#                 return False
+#         else:
+#             return True
+
+# 977. Squares of a Sorted Array
+class Solution(object):
+    def sortedSquares(self, nums, do=True):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums = [i ** 2 for i in nums] if do else nums
+        if len(nums) < 2:
+            return nums
         else:
-            return True
+            mid = nums.pop(len(nums) // 2)
+            left = [i for i in nums if i <= mid]
+            right = [i for i in nums if i > mid]
+            return self.sortedSquares(left, do=False) + [mid] + self.sortedSquares(right, do=False)
