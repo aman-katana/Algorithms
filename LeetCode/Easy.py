e596f8f57f22f94e2d6b37f67621d12b24847e69
Aman Katana <amanbek.kud@gmail.com>
2024-03-17 22:20:50 +0300

    LeetCode 28. Find the Index of the First Occurrence in a String

--- LeetCode/Easy.py
+++ LeetCode/Easy.py
@@ -1,56 +1,59 @@
 # 704. Binary Search ==================================================================================================
-# class Solution(object):
-#     def search(self, nums, target):
-#         """
-#         :type nums: List[int]
-#         :type target: int
-#         :rtype: int
-#         """
-#         start, end = 0, len(nums) - 1
-#
-#         while start <= end:
-#             mid = (start + end) // 2
-#             guess = nums[mid]
-#             if guess == target:
-#                 return mid
-#             elif guess > target:
-#                 end = mid - 1
-#             else:
-#                 start = mid + 1
-#         return -1
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+            guess = nums[mid]
+            if guess == target:
+                return mid
+            elif guess > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
+
 
 # 551. Student Attendance Record I
-# class Solution:
-#     def checkRecord(self, s: str) -> bool:
-#         absent = late = 0
-#         for i in s:
-#             if i == 'A':
-#                 absent += 1
-#                 late = 0
-#             elif i == 'L':
-#                 late += 1
-#             else:
-#                 late = 0
-#             if late == 3 or absent > 1:
-#                 return False
-#         else:
-#             return True
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        absent = late = 0
+        for i in s:
+            if i == 'A':
+                absent += 1
+                late = 0
+            elif i == 'L':
+                late += 1
+            else:
+                late = 0
+            if late == 3 or absent > 1:
+                return False
+        else:
+            return True
+
 
 # 977. Squares of a Sorted Array
-# class Solution(object):
-#     def sortedSquares(self, nums, do=True):
-#         """
-#         :type nums: List[int]
-#         :rtype: List[int]
-#         """
-#         nums = [i ** 2 for i in nums] if do else nums
-#         if len(nums) < 2:
-#             return nums
-#         else:
-#             mid = nums.pop(len(nums) // 2)
-#             left = [i for i in nums if i <= mid]
-#             right = [i for i in nums if i > mid]
-#             return self.sortedSquares(left, do=False) + [mid] + self.sortedSquares(right, do=False)
+class Solution(object):
+    def sortedSquares(self, nums, do=True):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums = [i ** 2 for i in nums] if do else nums
+        if len(nums) < 2:
+            return nums
+        else:
+            mid = nums.pop(len(nums) // 2)
+            left = [i for i in nums if i <= mid]
+            right = [i for i in nums if i > mid]
+            return self.sortedSquares(left, do=False) + [mid] + self.sortedSquares(right, do=False)
+
 
 # 58. Length of Last Word
 class Solution:
@@ -59,6 +62,18 @@ class Solution:
         end = len(s) - 1
         for i in range(end, -1, -1):
             if s[i] == ' ':
-                 return end - i
+                return end - i
 
         return end + 1
+
+
+# 28. Find the Index of the First Occurrence in a String
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(haystack) < len(needle):
+            return - 1
+
+        for i in range(len(haystack)):
+            if haystack[i: i + len(needle)] == needle:
+                return i
+        return -1
