983296b372ff4de27db8fe711c70814c38a8161c
Aman Katana <amanbek.kud@gmail.com>
2025-03-25 20:49:03 +0500

    LeetCode 176. Second Highest Salary | SQL

--- LeetCode/SQL.py
+++ LeetCode/SQL.py
@@ -1,7 +1,15 @@
 # 175. Combine Two Tables
-
 # SELECT firstName, lastName, city, state
 # FROM Person
 # LEFT JOIN Address ON Person.personID = Address.personID
 
 
+# 176. Second Highest Salary
+# WITH CTE_Salaries AS(
+#     SELECT DISTINCT salary
+#     FROM Employee
+#     ORDER BY salary DESC
+#     LIMIT 1, 1
+# )
+# SELECT IF(COUNT(salary)=1, salary, null) AS SecondHighestSalary
+# FROM CTE_Salaries
