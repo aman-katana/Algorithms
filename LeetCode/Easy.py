6fc90b81b0780d89cb19550f36f1b1056a11e1c6
Aman Katana <amanbek.kud@gmail.com>
2025-05-19 14:57:33 +0500

    35. Search Insert Position

--- LeetCode/Easy.py
+++ LeetCode/Easy.py
@@ -282,3 +282,20 @@ class Solution:
                     return False
 
         return len(q) == 0
+
+
+# 35. Search Insert Position
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] <= target:
+                start = mid + 1
+            elif nums[mid] > target:
+                end = mid - 1
+
+        return start
